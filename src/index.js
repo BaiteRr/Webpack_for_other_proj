fa842f62d38ea83f089471a0a6977ddda9ba296a
lysak.ilya <ilsy@brighta.biz>
2020-08-10 16:03:52 +0300

    img

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Post from './Post'
 import json from './assets/json'
+import WebpackLogo from './assets/webpack-logo.png'
 import './styles/style.css'
 
-const post = new Post('Webpack Title')
+const post = new Post('Webpack Title', WebpackLogo)
 
 console.log('Post to String', post.toString())
 
